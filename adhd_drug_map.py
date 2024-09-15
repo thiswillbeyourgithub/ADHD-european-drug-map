5068d55ebd6b0d3cd5ab027fc485fcabaf886648
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-15 14:41:41 +0200

    fix: colors were not green/red and were wrong for european wide authorizations

--- adhd_drug_map.py
+++ adhd_drug_map.py
@@ -161,14 +161,16 @@ def main(
     steps = []
     map = map.reset_index().set_index("index")
     colorscale = [[0, "rgb(255, 0, 0)"], [1, "rgb(0, 255, 0)"]]
-    colorscale = [
-        [0, '#FF0000'],    # Red
-        [0.33, '#FF0000'], # Red
-        [0.33, '#0000FF'], # Blue
-        [0.67, '#0000FF'], # Blue
-        [0.67, '#008000'], # Green
-        [1.0, '#008000']   # Green
-    ]
+    colorscale_all_1 = [[0, "rgb(0, 255, 0)"], [1, "rgb(0, 255, 0)"]]
+    colorscale_all_0 = [[0, "rgb(255, 0, 0)"], [1, "rgb(255, 0, 0)"]]
+    # colorscale = [
+    #     [0, '#FF0000'],    # Red
+    #     [0.33, '#FF0000'], # Red
+    #     [0.33, '#0000FF'], # Blue
+    #     [0.67, '#0000FF'], # Blue
+    #     [0.67, '#008000'], # Green
+    #     [1.0, '#008000']   # Green
+    # ]
 
 
     for i, medication in enumerate(drugs_country_list):
@@ -179,6 +181,16 @@ def main(
             text=[f"{a}<br>{b}" for a, b in zip(map["country"], map["medications"])],
             showscale=False,
         )
+        vals = list(set(map[f"color_{medication}"].tolist()))
+        if len(vals) == 1:
+            if vals[0] == 0:
+                fig.colorscale=colorscale_all_0
+            elif vals[0] == 1:
+                fig.colorscale=colorscale_all_1
+            else:
+                raise ValueError(vals)
+        else:
+            fig.colorscale=colorscale
         figs.add_trace(fig, row=1, col=1)
         step = dict(
             method="update",
